62486ef374f0d275c95d2e2e904e93f9bfabfc3c
Gelatart <daniel.f.wood@gmail.com>
2020-02-20 19:47:31 -0700

    Started adding functionality for creating employees
    At work, 2/20/2020

--- LabPhase1/LabPhase1/LabPhase1/Form1.cs
+++ LabPhase1/LabPhase1/LabPhase1/Form1.cs
@@ -16,5 +16,29 @@ namespace LabPhase1
         {
             InitializeComponent();
         }
+        
+        public void createEmployee() {
+            string id = firstName[0] + lastName[0] + "0";
+            //function that checks if id already taken?
+            string type; //grab from enum
+            string firstN;
+            string lastN;
+            string fullN = firstName + " " + lastName;
+            if(type == "Hourly") {
+                //hourlyRate
+                //hoursWorked
+                double rate;
+                double worked;
+                Hourly emp = new Hourly(id, firstN, lastN, fullN, rate, worked);
+            } else if (type == "Salary") {
+                //monthlySalary
+                double monthly;
+                Salary emp = new Salary(id, firstN, lastN, fullN, monthly);
+            } else if (type == "Sales") {
+                Sales emp = new Sales();
+            } else if (type == "Contract") {
+                Contract emp = new Contract();
+            }
+        }
     }
 }
